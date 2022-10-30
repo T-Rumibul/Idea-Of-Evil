7523608d0960a511455aff6c0a424a4ef3cbb77f
RynerNO <ryner.no@gmail.com>
2022-10-30 19:02:41 +0200

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM node:16-alpine
+RUN apk add g++ make py3-pip
 WORKDIR /
 COPY . .
 RUN npm install && npm run build
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,10 @@
 import './paths'
 import { run as runApi } from './api/';
 import { run as runBot } from './bot/';
-import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
 runApi();
 runBot();
-herokuAntiSleep();
 
-// Keeps heroku dyno awake
-function herokuAntiSleep() {
-	const port = process.env.PORT || 5001;
-	const herokuhost = process.env.HEROKU_HOST || `http://localhost:${port}`;
-	setInterval(() => {
-		axios.get(`${herokuhost}`).catch(() => {});
-		axios.get(`http://localhost:${port}`).catch(() => {});
-		axios
-			.post(`http://localhost:${port}`, {
-				adsadasda: 'adsadasd',
-			})
-			.catch(() => {});
-	}, 300000);
-}
+
