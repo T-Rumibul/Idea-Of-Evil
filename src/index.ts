cfe8a661c916d625589123b9e722a3710275a498
RynerNO <ryner.no@gmail.com>
2024-01-27 08:50:14 +0100

    added anisleep function to prevent Render from sleep

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import './paths';
 import dotenv from 'dotenv';
 import {run as runApi} from './api';
 import {run as runBot} from './bot';
-
+import LogManager from './utils/Logger';
 dotenv.config();
-
+const logger = new LogManager().getLogger('MAIN');
 runApi();
 runBot();
+function anisleepRender() {
+  fetch('https://io3.onrender.com', {
+    method: 'get',
+  }).then(response => {
+    logger.log(`Self Request Status: ${response.status}`);
+  });
+}
+setInterval(anisleepRender, 30000);
